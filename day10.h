d301a8e6bed6a03512a0c2646e10329b1f9f70cf
mbreit <markus.breit@gcsc.uni-frankfurt.de>
2017-12-14 07:27:39 +0100

    day 14

--- day10.h
+++ day10.h
@@ -6,6 +6,66 @@
  */
 
 
+std::string calculate_knot_hash(const std::string& line)
+{
+	size_t sz = line.size();
+
+	// convert to integer lengths
+	std::vector<int> vLength(sz);
+	for (size_t i = 0; i < sz; ++i)
+		vLength[i] = line[i];
+
+	vLength.reserve(sz+5);
+	vLength.push_back(17);
+	vLength.push_back(31);
+	vLength.push_back(73);
+	vLength.push_back(47);
+	vLength.push_back(23);
+	sz += 5;
+
+	// initiate list
+	int list[256];
+	for (size_t i = 0; i < 256; ++i)
+		list[i] = i;
+
+	// apply 64 iterations of knotting
+	int curPos = 0;
+	int skip = 0;
+	for (int k = 0; k < 64; ++k)
+	{
+		for (size_t i = 0; i < sz; ++i)
+		{
+			// reverse order
+			int n = vLength[i];
+			for (size_t j = 0; j < n/2; ++j)
+				std::swap(list[(curPos+j)%256], list[(curPos+n-1-j)%256]);
+
+			// move forward
+			curPos += (n + skip) % 256;
+			++skip;
+		}
+	}
+
+	// calculate dense hash
+	int denseHash[16];
+	for (size_t i = 0; i < 16; ++i)
+	{
+		int xorRes = 0;
+		for (int j = 0; j < 16; ++j)
+			xorRes ^= list[16*i + j];
+		denseHash[i] = xorRes;
+	}
+
+	// reformat dense hash to hexadecimal representation
+	std::ostringstream oss;
+	for (size_t i = 0; i < 16; ++i)
+		oss << std::setfill('0') << std::setw(2) << std::hex << denseHash[i];
+
+	return oss.str();
+}
+
+
+
 template <>
 void executeDay<10>(const std::string& fn)
 {
@@ -56,62 +116,8 @@ void executeDay<10>(const std::string& fn)
 		std::ifstream infile(fn.c_str());
 		std::string line;
 		std::getline(infile, line);
-		size_t sz = line.size();
 
-		// convert to integer lengths
-		std::vector<int> vLength(sz);
-		for (size_t i = 0; i < sz; ++i)
-			vLength[i] = line[i];
-
-		vLength.reserve(sz+5);
-		vLength.push_back(17);
-		vLength.push_back(31);
-		vLength.push_back(73);
-		vLength.push_back(47);
-		vLength.push_back(23);
-		sz += 5;
-
-		std::cout << vLength[0] << ", " << vLength[1] << std::endl;
-
-		// initiate list
-		int list[256];
-		for (size_t i = 0; i < 256; ++i)
-			list[i] = i;
-
-		// apply 64 iterations of knotting
-		int curPos = 0;
-		int skip = 0;
-		for (int k = 0; k < 64; ++k)
-		{
-			for (size_t i = 0; i < sz; ++i)
-			{
-				// reverse order
-				int n = vLength[i];
-				for (size_t j = 0; j < n/2; ++j)
-					std::swap(list[(curPos+j)%256], list[(curPos+n-1-j)%256]);
-
-				// move forward
-				curPos += (n + skip) % 256;
-				++skip;
-			}
-		}
-
-		// calculate dense hash
-		int denseHash[16];
-		for (size_t i = 0; i < 16; ++i)
-		{
-			int xorRes = 0;
-			for (int j = 0; j < 16; ++j)
-				xorRes ^= list[16*i + j];
-			denseHash[i] = xorRes;
-		}
-
-		// reformat dense hash to hexadecimal representation
-		std::ostringstream oss;
-		for (size_t i = 0; i < 16; ++i)
-			oss << std::setfill('0') << std::setw(2) << std::hex << denseHash[i];
-
-		solb = oss.str();
+		solb = calculate_knot_hash(line);
 	}
 
 	writeSolution(sola, solb);
--- day14.h
+++ day14.h
@@ -9,10 +9,157 @@
 template <>
 void executeDay<14>(const std::string& fn)
 {
-	std::ifstream infile(fn.c_str());
+	std::string input = "jxqlasbh";
 
+	size_t ones = 0;
+	std::vector<std::vector<size_t> > mem(128);
+	for (size_t i = 0; i < 128; ++i)
+		mem[i].resize(128, 0);
 
-	writeSolution((int) 0, (int) 0);
+	for (size_t i = 0; i < 128; ++i)
+	{
+		std::ostringstream oss;
+		oss << input << "-" << i;
+
+		std::string knotHash = calculate_knot_hash(oss.str());
+
+		// count ones in knot hash
+		std::vector<size_t>& row = mem[i];
+		for (size_t j = 0; j < 32; ++j)
+		{
+			switch (knotHash[j])
+			{
+				case '0':
+					break;
+				case '1':
+					ones += 1;
+					row[4*j+3] = 1;
+					break;
+				case '2':
+					ones += 1;
+					row[4*j+2] = 1;
+					break;
+				case '3':
+					ones += 2;
+					row[4*j+3] = 1;
+					row[4*j+2] = 1;
+					break;
+				case '4':
+					ones += 1;
+					row[4*j+1] = 1;
+					break;
+				case '5':
+					ones += 2;
+					row[4*j+3] = 1;
+					row[4*j+1] = 1;
+					break;
+				case '6':
+					ones += 2;
+					row[4*j+2] = 1;
+					row[4*j+1] = 1;
+					break;
+				case '7':
+					ones += 3;
+					row[4*j+3] = 1;
+					row[4*j+2] = 1;
+					row[4*j+1] = 1;
+					break;
+				case '8':
+					ones += 1;
+					row[4*j] = 1;
+					break;
+				case '9':
+					ones += 2;
+					row[4*j+3] = 1;
+					row[4*j] = 1;
+					break;
+				case 'a':
+					ones += 2;
+					row[4*j+2] = 1;
+					row[4*j] = 1;
+					break;
+				case 'b':
+					ones += 3;
+					row[4*j+3] = 1;
+					row[4*j+2] = 1;
+					row[4*j] = 1;
+					break;
+				case 'c':
+					ones += 2;
+					row[4*j+1] = 1;
+					row[4*j] = 1;
+					break;
+				case 'd':
+					ones += 3;
+					row[4*j+3] = 1;
+					row[4*j+1] = 1;
+					row[4*j] = 1;
+					break;
+				case 'e':
+					ones += 3;
+					row[4*j+2] = 1;
+					row[4*j+1] = 1;
+					row[4*j] = 1;
+					break;
+				case 'f':
+					ones += 4;
+					row[4*j+3] = 1;
+					row[4*j+2] = 1;
+					row[4*j+1] = 1;
+					row[4*j] = 1;
+					break;
+				default:
+					std::cout << "Unexpected character in knot hash." << std::endl;
+					throw;
+			}
+		}
+	}
+
+	// analyze contiguity
+	size_t regCnt = 1;
+	for (size_t i = 0; i < 128; ++i)
+	{
+		for (size_t j = 0; j < 128; ++j)
+		{
+			if (mem[i][j] != 1)
+				continue;
+
+			// start a new region
+			++regCnt;
+			std::queue<std::pair<size_t, size_t> > q;
+			q.push(std::make_pair(i,j));
+
+			// find whole region
+			while (!q.empty())
+			{
+				const std::pair<size_t, size_t>& p = q.front();
+				size_t k = p.first;
+				size_t l = p.second;
+				q.pop();
+
+				// check for 1
+				if (mem[k][l] != 1)
+					continue;
+
+				// assign region
+				mem[k][l] = regCnt;
+
+				// look left, right, top, bottom
+				if (k > 0)
+					q.push(std::make_pair(k-1, l));
+				if (k < 127)
+					q.push(std::make_pair(k+1, l));
+				if (l > 0)
+					q.push(std::make_pair(k, l-1));
+				if (l < 127)
+					q.push(std::make_pair(k, l+1));
+			}
+		}
+	}
+
+	--regCnt;
+
+	writeSolution(ones, regCnt);
 }
 
 
