ca7d8e4483fe337dd0b51189f327123cd20a5801
mbreit <markus.breit@gcsc.uni-frankfurt.de>
2017-12-23 08:29:19 +0100

    days 22 and 23

--- day22.h
+++ day22.h
@@ -11,8 +11,106 @@ void executeDay<22>(const std::string& fn)
 {
 	std::ifstream infile(fn.c_str());
 
+	std::map<std::pair<int, int>, bool> mem;
 
-	writeSolution((int) 0, (int) 0);
+	std::string line;
+	int row = 0;
+	int col = 0;
+	while (std::getline(infile, line))
+	{
+		for (col = 0; col < (int) line.size(); ++col)
+			if (line[col] == '#')
+				mem[std::make_pair(row, col)] = true;
+			else
+				mem[std::make_pair(row, col)] = false;
+
+		++row;
+	}
+
+	col = row / 2;
+	row = col;
+
+	int dir = 0;
+	int infCnt = 0;
+	std::pair<int, int> entry = std::make_pair(row, col);
+	for (int i = 0; i < 10000; ++i)
+	{
+		if (mem[entry])
+			dir = (dir+1)%4;
+		else
+		{
+			dir = (dir+3)%4;
+			++infCnt;
+		}
+		mem[entry] = !mem[entry];
+
+		switch (dir)
+		{
+			case 0: entry.first -= 1; break;
+			case 1: entry.second += 1; break;
+			case 2: entry.first += 1; break;
+			case 3: entry.second -= 1; break;
+			default: std::cout << "bad direction " << dir << std::endl; throw;
+		}
+	}
+
+
+	std::map<std::pair<int, int>, int> mem2;
+
+	row = 0;
+	col = 0;
+	std::ifstream infile2(fn.c_str());
+	while (std::getline(infile2, line))
+	{
+		for (col = 0; col < (int) line.size(); ++col)
+			if (line[col] == '#')
+				mem2[std::make_pair(row, col)] = 3;
+			else
+				mem2[std::make_pair(row, col)] = 0;
+
+		++row;
+	}
+
+	col = row / 2;
+	row = col;
+
+	dir = 0;
+	int infCnt2 = 0;
+	entry = std::make_pair(row, col);
+	for (int i = 0; i < 10000000; ++i)
+	{
+		if (mem2[entry] == 3)
+		{
+			dir = (dir+1)%4;
+			mem2[entry] = 1;
+		}
+		else if (mem2[entry] == 0)
+		{
+			dir = (dir+3)%4;
+			mem2[entry] = 2;
+		}
+		else if (mem2[entry] == 1)
+		{
+			dir = (dir+2)%4;
+			mem2[entry] = 0;
+		}
+		else if (mem2[entry] == 2)
+		{
+			mem2[entry] = 3;
+			++infCnt2;
+		}
+
+		switch (dir)
+		{
+			case 0: entry.first -= 1; break;
+			case 1: entry.second += 1; break;
+			case 2: entry.first += 1; break;
+			case 3: entry.second -= 1; break;
+			default: std::cout << "bad direction " << dir << std::endl; throw;
+		}
+	}
+
+	writeSolution(infCnt, infCnt2);
 }
 
 
--- day23.h
+++ day23.h
@@ -11,8 +11,152 @@ void executeDay<23>(const std::string& fn)
 {
 	std::ifstream infile(fn.c_str());
 
+	std::string line;
 
-	writeSolution((int) 0, (int) 0);
+	std::vector<std::string> vInstr;
+	vInstr.reserve(1000);
+
+	// read data
+	while (std::getline(infile, line))
+		vInstr.push_back(line);
+	size_t sz = vInstr.size();
+
+	// part a
+	int sola = 0;
+	{
+		std::vector<int64_t> reg(8, 0);
+
+		size_t sz = vInstr.size();
+		int pos = 0;
+		int lastFreq = 0;
+		while (pos >= 0 && pos < (int) sz)
+		{
+			std::istringstream iss(vInstr[pos]);
+			std::string instr;
+
+	//std::cout << vInstr[pos] << std::endl;
+
+			iss >> instr;
+
+
+
+
+			std::string next;
+			iss >> next;
+
+			int arg1I;
+			bool arg1IsReg = true;
+			if (next[0] - 'a' < 8 && next[0] - 'a' >= 0)
+			{
+				arg1I = next[0] - 'a';
+				arg1IsReg = true;
+			}
+			else
+			{
+				arg1I = std::stoi(next);
+				arg1IsReg = false;
+			}
+
+
+
+			int arg2I;
+			bool arg2IsReg = false;
+			iss >> next;
+			if (next[0] - 'a' < 8 && next[0] - 'a' >= 0)
+			{
+				arg2I = next[0] - 'a';
+				arg2IsReg = true;
+			}
+			else
+			{
+				arg2I = std::stoi(next);
+				arg2IsReg = false;
+			}
+
+//std::cout << instr << " " << (arg1IsReg ? "r" : "") << arg1I << " " << (arg2IsReg ? "r" : "") << arg2I << std::endl;
+
+
+
+
+			if (instr == "set")
+			{
+				if (arg2IsReg)
+					reg[arg1I] = reg[arg2I];
+				else
+					reg[arg1I] = arg2I;
+			}
+			else if (instr == "sub")
+			{
+				if (arg2IsReg)
+					reg[arg1I] = reg[arg1I] - reg[arg2I];
+				else
+					reg[arg1I] = reg[arg1I] - arg2I;
+			}
+			else if (instr == "mul")
+			{
+				if (arg2IsReg)
+					reg[arg1I] = reg[arg1I] * reg[arg2I];
+				else
+					reg[arg1I] = reg[arg1I] * arg2I;
+
+				++sola;
+			}
+			else if (instr == "jnz")
+			{
+				if (arg1IsReg)
+				{
+					if (reg[arg1I] != 0)
+					{
+						if (arg2IsReg)
+							pos = pos + reg[arg2I] - 1;
+						else
+							pos = pos + arg2I - 1;
+					}
+				}
+				else
+				{
+					if (arg1I != 0)
+					{
+						if (arg2IsReg)
+							pos = pos + reg[arg2I] - 1;
+						else
+							pos = pos + arg2I - 1;
+					}
+				}
+			}
+			else throw;
+
+			++pos;
+		}
+	}
+
+	// part b: find non-prime numbers in [108100, 125100]
+	int last = 354;
+	std::vector<bool> sieve(355, true);
+	std::vector<bool> zone(17001, true);
+	for (int i = 2; i <= last; ++i)
+	{
+		if (!sieve[i])
+			continue;
+
+		// shoot in sieve
+		for (int j = 2*i; j <= last; j += i)
+			sieve[j] = false;
+
+		// shoot in zone
+		int rest = 108100 % i;
+		int start = (i - rest) % i;
+
+		for (int j = start; j < 17001; j += i)
+			zone[j] = false;
+	}
+
+	size_t cnt = 0;
+	for (size_t i = 0; i < 17001; i += 17)
+		if (!zone[i])
+			++cnt;
+
+	writeSolution(sola, cnt);
 }
 
 
