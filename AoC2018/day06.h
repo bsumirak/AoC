93344f907fb94ffd09a4ce62b1d6eac278a9d28c
mbreit <markus.breit@gcsc.uni-frankfurt.de>
2018-12-06 08:03:47 +0100

    day 06

--- AoC2018/day06.h
+++ AoC2018/day06.h
@@ -1,7 +1,7 @@
 /*
  * day06.h
  *
- *  Created on: 06.12.2017
+ *  Created on: 2017-12-06
  *      Author: mbreit
  */
 
@@ -9,71 +9,65 @@
 template <>
 void executeDay<6>(const std::string& fn)
 {
-	// part a and b together (and really brute force)
-	int tmp[] = {14, 0, 15, 12, 11, 11, 3, 5, 1, 6, 8, 4, 9, 1, 8, 4};
-	std::vector<int> mem(tmp, tmp+16);
-	std::vector<std::vector<int> > allStates;
-	allStates.reserve(1000);
-	allStates.push_back(mem);
-
-	int na = 1;
-	int nb;
-	while (true)
+	// read input
+	std::ifstream infile(fn.c_str());
+	std::string line;
+	std::vector<std::pair<int, int> > coords(50);
+	std::size_t i = 0;
+	while (std::getline(infile, line))
 	{
-		// find slot with max.
-		int max = mem[0];
-		size_t maxInd = 0;
-		for (size_t i = 1; i < 16; ++i)
-		{
-			if (mem[i] > max)
-			{
-				max = mem[i];
-				maxInd = i;
-			}
-		}
-
-		// distribute
-		int forAll = max / 16;
-		int rest = max % 16;
-
-		for (size_t i = 0; i < 16; ++i)
-			mem[i] += forAll;
-
-		for (size_t i = maxInd+1; i < maxInd+1+rest; ++i)
-			++mem[i%16];
-
-		mem[maxInd] = forAll;
-
-		// check reoccurrence
-		bool reocc = false;
-		for (size_t i = 0; i < allStates.size(); ++i)
-		{
-			bool same = true;
-			for (size_t j = 0; j < 16; ++j)
-			{
-				if (allStates[i][j] != mem[j])
-				{
-					same = false;
-					break;
-				}
-			}
-
-			if (same)
-			{
-				reocc = true;
-				nb = na - i;
-				break;
-			}
-		}
-
-		if (reocc) break;
-
-		// save and inc
-		allStates.push_back(mem);
-		++na;
+		std::istringstream iss(line);
+		iss >> coords[i].first;
+		iss >> coords[i].second;
+		++i;
 	}
 
-	writeSolution(na, nb);
+	// part a and b together
+	std::vector<bool> invalid(50, false);
+	std::vector<int> cnt(50,0);
+	size_t numTotalDistLessThan10000 = 0;
+	for (int i = 0; i < 800; ++i)
+	{
+		for (int j = 0; j < 800; ++j)
+		{
+			bool tied = false;
+			int minDist = 20000;
+			size_t minK = 0;
+			size_t totalDist = 0;
+			for (size_t k = 0; k < 50; ++k)
+			{
+				int dist = abs(coords[k].first - (i-200)) + abs(coords[k].second - (j-200));
+				if (dist == minDist)
+					tied = true;
+				if (dist < minDist)
+				{
+					tied = false;
+					minDist = dist;
+					minK = k;
+				}
+				totalDist += dist;
+			}
+
+			if (!tied)
+				++cnt[minK];
+
+			// invalidate border owners (for they have infinite size)
+			if (i == 0 || i == 799 || j == 0 || j == 799)
+				invalid[minK] = true;
+
+			if (totalDist < 10000)
+				++numTotalDistLessThan10000;
+		}
+	}
+
+	size_t maxSize = 0;
+	for (size_t k = 0; k < 50; ++k)
+	{
+		if (cnt[k] > maxSize && !invalid[k])
+			maxSize = cnt[k];
+	}
+
+	writeSolution(maxSize, numTotalDistLessThan10000);
 }
 
 
--- AoC2018/main.cpp
+++ AoC2018/main.cpp
@@ -31,14 +31,16 @@ template <typename A, typename B>
 void writeSolution(A a, B b);
 
 
+/*
 #include "day01.h"
 #include "day02.h"
 #include "day03.h"
 #include "day04.h"
 #include "day05.h"
+*/
 #include "day06.h"
-/*#include "day07.h"
-#include "day08.h"
+#include "day07.h"
+/*#include "day08.h"
 #include "day09.h"
 #include "day10.h"
 #include "day11.h"
