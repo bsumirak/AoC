8eeb2f95fceccc6a7cbe7598101fc45ed7cb323a
mbreit <markus.breit@gcsc.uni-frankfurt.de>
2018-12-08 12:41:02 +0100

    day 07

--- AoC2018/day07.h
+++ AoC2018/day07.h
@@ -5,69 +5,6 @@
  *      Author: mbreit
  */
 
-struct node7
-{
-	~node7()
-	{
-		size_t sz = children.size();
-		for (size_t i = 0; i < sz; ++i)
-			delete children[i];
-	}
-
-	std::string name;
-	std::vector<node7*> children;
-	node7* parent;
-	int weight;
-	int totalWeight;
-};
-
-
-void build_tree7
-(
-	std::map<std::string, int>& weights,
-	std::map<std::string, std::vector<std::string> >& children,
-	std::map<std::string, std::string>& parent,
-	node7* root
-)
-{
-	const std::vector<std::string>& vCh = children[root->name];
-	size_t sz = vCh.size();
-	for (size_t i = 0; i < sz; ++i)
-	{
-		// set name, weight and parent by parent
-		node7* ch = new node7();
-		ch->name = vCh[i];
-		ch->weight = weights[ch->name];
-		ch->parent = root;
-
-		// add child to parent
-		root->children.push_back(ch);
-
-		// recurse
-		build_tree7(weights, children, parent, ch);
-	}
-
-	// add weights of children to totalWeight
-	root->totalWeight = root->weight;
-	for (size_t i = 0; i < sz; ++i)
-		root->totalWeight += root->children[i]->totalWeight;
-}
-
-bool isEquilibrated(node7* node)
-{
-	size_t sz = node->children.size();
-	if (!sz) return true;
-
-	int w = node->children[0]->totalWeight;
-	for (size_t i = 1; i < sz; ++i)
-	{
-		if (node->children[i]->totalWeight != w)
-			return false;
-	}
-	return true;
-}
-
-
 
 
 template <>
@@ -75,130 +12,160 @@ void executeDay<7>(const std::string& fn)
 {
 	// read input
 	std::ifstream infile(fn.c_str());
-	std::string line;
-	std::map<std::string, int> weights;
-	std::map<std::string, std::vector<std::string> > children;
-	std::map<std::string, std::string> parent;
-
-	while (std::getline(infile, line))
+	std::map<char, std::vector<char> > deps;
+	char first, second;
+	while (infile >> first)
 	{
-		std::istringstream iss(line);
+		infile >> second;
+		deps[second].push_back(first);
+		deps[first];
+	}
 
-		std::set<std::string> passphrase;
-		std::string tmp;
-		bool valid = true;
+	std::map<char, std::vector<char> > copyDeps(deps);
 
-		std::string name;
-		iss >> name;
-		std::string weight;
-		iss >> weight;
-		tmp = weight.substr(1, weight.size()-2);
-		int w = atoi(tmp.c_str());
+	// part a
+	std::string order;
+	std::map<char, std::vector<char> >::const_iterator it = deps.begin();
+	std::map<char, std::vector<char> >::const_iterator itEnd = deps.end();
 
-		weights[name] = w;
+	std::map<char, bool> used;
+	for (; it != itEnd; ++it)
+		used[it->first] = false;
 
-		int cnt = 0;
-		while (iss >> tmp)
+	it = deps.begin();
+	while (it != itEnd)
+	{
+		if (it->second.size() || used[it->first])
 		{
-			// ignore "->"
-			if (!cnt)
+			++it;
+			continue;
+		}
+
+		char c = it->first;
+		used[c] = true;
+		order += c;
+
+		std::map<char, std::vector<char> >::iterator it1 = deps.begin();
+		for (; it1 != itEnd; ++it1)
+		{
+			const std::size_t sz = it1->second.size();
+			for (std::size_t i = 0; i < sz; ++i)
+				if (it1->second[i] == c)
+				{
+					it1->second.erase(it1->second.begin()+i);
+					break;
+				}
+		}
+
+		it = deps.begin();
+	}
+
+
+	// part b
+	it = copyDeps.begin();
+	itEnd = copyDeps.end();
+
+	std::map<char, bool> workedOn;
+	for (; it != itEnd; ++it)
+	{
+		used[it->first] = false;
+		workedOn[it->first] = false;
+	}
+	int time = -1;
+	int finishes[5] = {0,0,0,0,0};
+	char curJob[5] = {'0', '0', '0', '0', '0'};
+	while (true)
+	{
+		// who is finishing?
+		int finisher = 0;
+		int minFinish = 3000;
+		for (int i = 0; i < 5; ++i)
+			if (finishes[i] > time && finishes[i] < minFinish)
+				minFinish = finishes[i];
+
+		std::cout << minFinish << std::endl;
+		time = minFinish;
+
+		// all finishers' jobs are done
+		for (int i = 0; i < 5; ++i)
+		{
+			if (finishes[i] == minFinish)
 			{
-				++cnt;
+				char c = curJob[i];
+				//used[curJob[i]] = true;
+
+				std::map<char, std::vector<char> >::iterator it1 = copyDeps.begin();
+				for (; it1 != itEnd; ++it1)
+				{
+					const std::size_t sz = it1->second.size();
+					for (std::size_t i = 0; i < sz; ++i)
+						if (it1->second[i] == c)
+						{
+							it1->second.erase(it1->second.begin()+i);
+							break;
+						}
+				}
+
+				curJob[i] = '0';
+			}
+		}
+
+		// all unoccupied workers can take new job
+		int nextWorker = 0;
+		it = copyDeps.begin();
+		bool foundNewJob = false;
+		while (nextWorker < 5)
+		{
+			if (curJob[nextWorker] != '0')
+			{
+				std::cout << "worker " << nextWorker << " is occupied" << std::endl;
+				++nextWorker;
 				continue;
 			}
 
-			// remove comma
-			if (tmp.c_str()[tmp.size()-1] == ',')
-				tmp = tmp.substr(0,tmp.size()-1);
+			while (it != itEnd && (it->second.size() || used[it->first]))
+				++it;
 
-			children[name].push_back(tmp);
-			parent[tmp] = name;
-		}
-	}
-
-	// part a
-	std::map<std::string, std::string>::const_iterator it = parent.begin();
-	while (parent.find(it->second) != parent.end())
-		it = parent.find(it->second);
-	std::string rootName = it->second;
-
-	// part b
-
-	// build a tree
-	node7 root;
-	root.name = rootName;
-	root.weight = weights[rootName];
-	root.parent = NULL;
-	build_tree7(weights, children, parent, &root);
-
-	// find deepest uneven weight
-	node7* node = &root;
-	while (true)
-	{
-		bool equiChild = true;
-		size_t sz = node->children.size();
-		for (size_t i = 0; i < sz; ++i)
-		{
-			if (!isEquilibrated(node->children[i]))
+			// no more jobs
+			if (it == itEnd)
 			{
-				node = node->children[i];
-				equiChild = false;
+				std::cout << "worker " << nextWorker << " is idle from time "
+					<< time << " onwards" << std::endl;
 				break;
 			}
+
+			foundNewJob = true;
+			curJob[nextWorker] = it->first;
+			finishes[nextWorker] = time + 60 + it->first - 'A' + 1;
+			used[it->first] = true;
+
+			std::cout << "worker " << nextWorker << " takes job "
+				<< it->first << " at time " << time << std::endl;
+
+			++it;
+			++nextWorker;
 		}
 
-		if (equiChild)
-			break;
-	}
-
-	// now we are in the child with the wrong weight
-
-	if (node->children.size() == 2)
-		std::cout << "This case is not implemented." << std::cout;
-
-	size_t culprit;
-	size_t sz = node->children.size();
-	for (size_t i = 0; i < sz-1; ++i)
-	{
-		if (node->children[i]->totalWeight != node->children[i+1]->totalWeight)
+		if (!foundNewJob)
 		{
-			// which one of the two is the culprit?
-			if (i > 0)
+			bool workersOccupied = false;
+			for (int i = 0; i < 5; ++i)
 			{
-				if (node->children[i]->totalWeight != node->children[0]->totalWeight)
-					culprit = i;
-				else
-					culprit = i+1;
-			}
-			else
-			{
-				if (node->children[0]->totalWeight != node->children[2]->totalWeight)
-					culprit = 0;
-				else
-					culprit = 1;
+				if (curJob[i] != '0')
+				{
+					workersOccupied = true;
+					break;
+				}
 			}
 
-			break;
+			if (!workersOccupied)
+				break;
 		}
 	}
 
-	int totW = node->children[culprit]->totalWeight;
-	int reqTotW = node->children[(culprit+1) % 2]->totalWeight;
 
-	int reqW = reqTotW - (node->children[culprit]->totalWeight - node->children[culprit]->weight);
 
-	/*
-	std::cout << "unequilibrated node: " << node->name << std::endl;
-	std::cout << "  with children: ";
-	for (size_t i = 0; i < sz; ++i)
-		std::cout << node->children[i]->name << " (" << node->children[i]->totalWeight << ")   ";
-	std::cout << std::endl;
-	std::cout << "culprit: " << node->children[culprit]->name << " with own weight "
-		<< node->children[culprit]->weight << std::endl;
-	std::cout << std::endl;
-	*/
-
-	writeSolution(rootName, reqW);
+	writeSolution(order, time);
 }
 
 
