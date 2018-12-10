877880a1f6e24bc3d5ee802ce98d8edb50c85bf1
mbreit <markus.breit@gcsc.uni-frankfurt.de>
2018-12-10 07:33:49 +0100

    day 10

--- AoC2018/day10.h
+++ AoC2018/day10.h
@@ -1,126 +1,88 @@
 /*
  * day10.h
  *
- *  Created on: 10.12.2017
+ *  Created on: 2018-12-10
  *      Author: mbreit
  */
-
-
-std::string calculate_knot_hash(const std::string& line)
-{
-	size_t sz = line.size();
-
-	// convert to integer lengths
-	std::vector<int> vLength(sz);
-	for (size_t i = 0; i < sz; ++i)
-		vLength[i] = line[i];
-
-	vLength.reserve(sz+5);
-	vLength.push_back(17);
-	vLength.push_back(31);
-	vLength.push_back(73);
-	vLength.push_back(47);
-	vLength.push_back(23);
-	sz += 5;
-
-	// initiate list
-	int list[256];
-	for (size_t i = 0; i < 256; ++i)
-		list[i] = i;
-
-	// apply 64 iterations of knotting
-	int curPos = 0;
-	int skip = 0;
-	for (int k = 0; k < 64; ++k)
-	{
-		for (size_t i = 0; i < sz; ++i)
-		{
-			// reverse order
-			int n = vLength[i];
-			for (size_t j = 0; j < n/2; ++j)
-				std::swap(list[(curPos+j)%256], list[(curPos+n-1-j)%256]);
-
-			// move forward
-			curPos += (n + skip) % 256;
-			++skip;
-		}
-	}
-
-	// calculate dense hash
-	int denseHash[16];
-	for (size_t i = 0; i < 16; ++i)
-	{
-		int xorRes = 0;
-		for (int j = 0; j < 16; ++j)
-			xorRes ^= list[16*i + j];
-		denseHash[i] = xorRes;
-	}
-
-	// reformat dense hash to hexadecimal representation
-	std::ostringstream oss;
-	for (size_t i = 0; i < 16; ++i)
-		oss << std::setfill('0') << std::setw(2) << std::hex << denseHash[i];
-
-	return oss.str();
-}
-
 
 
 template <>
 void executeDay<10>(const std::string& fn)
 {
 	// part a
-	int sola;
-	{
-		std::ifstream infile(fn.c_str());
-		std::string line;
-		std::getline(infile, line);
-		std::istringstream iss(line);
+	int solb;
 
-		// read file
-		std::vector<int> vLength;
-		std::string token;
-		while (std::getline(iss, token, ','))
+	// read input
+	std::ifstream infile(fn.c_str());
+
+	std::vector<std::pair<int, int> > vPos;
+	std::vector<std::pair<int, int> > vVel;
+	int x1, x2;
+	while (infile >> x1)
+	{
+		infile >> x2;
+		vPos.push_back(std::make_pair(x2, x1));
+
+		infile >> x1;
+		infile >> x2;
+		vVel.push_back(std::make_pair(x2, x1));
+	}
+	const int sz = vPos.size();
+
+
+	// part a and b together
+	for (int i = 0; i < 11000; ++i)
+	{
+		int minX = 100000;
+		int maxX = -100000;
+		int minY = 100000;
+		int maxY = -100000;
+
+		for (int k = 0; k < sz; ++k)
 		{
-			std::istringstream tokenStream(token);
-			int i;
-			tokenStream >> i;
-			vLength.push_back(i);
+			vPos[k].first += vVel[k].first;
+			vPos[k].second += vVel[k].second;
+
+			if (vPos[k].first < minY)
+				minY = vPos[k].first;
+			if (vPos[k].first > maxY)
+				maxY = vPos[k].first;
+			if (vPos[k].second < minX)
+				minX = vPos[k].second;
+			if (vPos[k].second > maxX)
+				maxX = vPos[k].second;
 		}
 
-		int list[256];
-		for (size_t i = 0; i < 256; ++i)
-			list[i] = i;
-
-		int curPos = 0;
-		int skip = 0;
-		size_t sz = vLength.size();
-		for (size_t i = 0; i < sz; ++i)
+		if (maxY - minY < 15)
 		{
-			// reverse order
-			int n = vLength[i];
-			for (size_t j = 0; j < n/2; ++j)
-				std::swap(list[(curPos+j)%256], list[(curPos+n-1-j)%256]);
+			std::sort(vPos.begin(), vPos.end());
 
-			// move forward
-			curPos += (n + skip) % 256;
-			++skip;
+			int cur = 0;
+			for (int y = minY; y <= maxY; ++y)
+			{
+				for (int x = minX; x <= maxX; ++x)
+				{
+					if (vPos[cur].first == y && vPos[cur].second == x)
+					{
+						std::cout << "#";
+						while (cur < sz && vPos[cur].first == y && vPos[cur].second == x)
+							++cur;
+					}
+					else
+					{
+						std::cout << " ";
+					}
+				}
+				std::cout << std::endl;
+			}
+
+			solb = i + 1;
+			break;
 		}
-
-		sola = list[0] * list[1];
 	}
 
-	// part b
-	std::string solb;
-	{
-		std::ifstream infile(fn.c_str());
-		std::string line;
-		std::getline(infile, line);
 
-		solb = calculate_knot_hash(line);
-	}
-
-	writeSolution(sola, solb);
+	writeSolution("", solb);
 }
 
 
