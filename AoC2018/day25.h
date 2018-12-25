e920949a658a1850af84afb31bd586ebd4ca8651
mbreit <markus.breit@gcsc.uni-frankfurt.de>
2018-12-25 10:30:49 +0100

    day 25

--- AoC2018/day25.h
+++ AoC2018/day25.h
@@ -1,118 +1,73 @@
 /*
  * day25.h
  *
- *  Created on: 25.12.2017
+ *  Created on: 2018-12-25
  *      Author: mbreit
  */
 
 
+struct vector4
+{
+	int coord[4];
+};
+
 template <>
 void executeDay<25>(const std::string& fn)
 {
-	int state = 0;
-	int pos = 0;
+	std::ifstream infile(fn.c_str());
+	std::vector<vector4> coords;
 
-	std::map<int, bool> tape;
-
-	for (size_t i = 0; i < 12656374; ++i)
+	// part a
+	vector4 tmp;
+	while (infile >> tmp.coord[0])
 	{
-		switch (state)
+		infile >> tmp.coord[1];
+		infile >> tmp.coord[2];
+		infile >> tmp.coord[3];
+		coords.push_back(tmp);
+	}
+	const size_t sz = coords.size();
+	std::vector<std::vector<vector4> > constell;
+
+	for (size_t i = 0; i < sz; ++i)
+	{
+		std::vector<size_t> merge;
+		for (size_t j = 0; j < constell.size(); ++j)
 		{
-		case 0:
-			if (!tape[pos])
+			std::vector<vector4>& c = constell[j];
+			for (size_t k = 0; k < c.size(); ++k)
 			{
-				tape[pos] = 1;
-				++pos;
-				state = 1;
+				if (abs(c[k].coord[0]-coords[i].coord[0])
+						+ abs(c[k].coord[1]-coords[i].coord[1])
+						+ abs(c[k].coord[2]-coords[i].coord[2])
+						+ abs(c[k].coord[3]-coords[i].coord[3]) <= 3)
+				{
+					c.push_back(coords[i]);
+					merge.push_back(j);
+					break;
+				}
 			}
-			else
+		}
+		if (merge.size())
+		{
+			constell[merge[0]].push_back(coords[i]);
+			for (size_t j = 1; j < merge.size(); ++j)
 			{
-				tape[pos] = 0;
-				--pos;
-				state = 2;
+				for (size_t k = 0; k < constell[merge[j]].size(); ++k)
+					constell[merge[0]].push_back(constell[merge[j]][k]);
 			}
-			break;
-		case 1:
-			if (!tape[pos])
-			{
-				tape[pos] = 1;
-				--pos;
-				state = 0;
-			}
-			else
-			{
-				tape[pos] = 1;
-				--pos;
-				state = 3;
-			}
-			break;
-		case 2:
-			if (!tape[pos])
-			{
-				tape[pos] = 1;
-				++pos;
-				state = 3;
-			}
-			else
-			{
-				tape[pos] = 0;
-				++pos;
-				state = 2;
-			}
-			break;
-		case 3:
-			if (!tape[pos])
-			{
-				tape[pos] = 0;
-				--pos;
-				state = 1;
-			}
-			else
-			{
-				tape[pos] = 0;
-				++pos;
-				state = 4;
-			}
-			break;
-		case 4:
-			if (!tape[pos])
-			{
-				tape[pos] = 1;
-				++pos;
-				state = 2;
-			}
-			else
-			{
-				tape[pos] = 1;
-				--pos;
-				state = 5;
-			}
-			break;
-		case 5:
-			if (!tape[pos])
-			{
-				tape[pos] = 1;
-				--pos;
-				state = 4;
-			}
-			else
-			{
-				tape[pos] = 1;
-				++pos;
-				state = 0;
-			}
-			break;
+
+			for (size_t j = merge.size() -1; j > 0; --j)
+				constell.erase(constell.begin() + merge[j]);
+		}
+		else
+		{
+			constell.push_back(std::vector<vector4>(1));
+			constell.back().back() = coords[i];
 		}
 	}
 
-	size_t cnt = 0;
-	std::map<int, bool>::const_iterator it = tape.begin();
-	std::map<int, bool>::const_iterator itEnd = tape.end();
-	for (; it != itEnd; ++it)
-		if (it->second)
-			++cnt;
-
-	writeSolution(cnt, (int) 0);
+	writeSolution(constell.size(), 0);
 }
 
 
