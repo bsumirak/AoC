762342eed80b534e3f83d897c1a1a58536c2d79d
mbreit <markus.breit@gcsc.uni-frankfurt.de>
2019-12-02 06:36:06 +0100

    day 01

--- AoC2019/day01.h
+++ AoC2019/day01.h
@@ -10,41 +10,24 @@ template <>
 void executeDay<1>(const std::string& fn)
 {
 	std::ifstream infile(fn.c_str());
-	std::vector<int> list;
 	int i;
 
 	// part a
 	int suma = 0;
+	int sumb = 0;
 	while (infile >> i)
 	{
+		i = i/3 - 2;
 		suma += i;
-		list.push_back(i);
-	}
-
-	// part b
-	int b = 0;
-	size_t l = list.size();
-	std::vector<int> vList(1,0);
-	while (true)
-	{
-		for (size_t k = 0; k < l; ++k)
+		while (i > 8)
 		{
-			int newVal = vList.back() + list[k];
-			const size_t vListSz = vList.size();
-			for (size_t j = 0; j < vListSz; ++j)
-			{
-				if (vList[j] == newVal)
-				{
-					b = newVal;
-					goto result;
-				}
-			}
-			vList.push_back(newVal);
+			i /= 3;
+			i -= 2;
+			sumb += i;
 		}
 	}
 
-result:
-	writeSolution(suma, b);
+	writeSolution(suma, suma + sumb);
 }
 
 
