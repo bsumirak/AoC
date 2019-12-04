b1cfa4e4313d02530e492c6647c7d99699ad0f03
mbreit <markus.breit@gcsc.uni-frankfurt.de>
2019-12-04 07:15:23 +0100

    day 04

--- AoC2019/day04.h
+++ AoC2019/day04.h
@@ -1,7 +1,7 @@
 /*
  * day04.h
  *
- *  Created on: 04.12.2017
+ *  Created on: 2019-12-04
  *      Author: mbreit
  */
 
@@ -9,107 +9,74 @@
 template <>
 void executeDay<4>(const std::string& fn)
 {
-	std::ifstream infile(fn.c_str());
-	std::string line;
-	int suma = 0;
-	int sumb = 0;
-	while (std::getline(infile, line))
+	std::vector<int> digits(6);
+	int cnt1 = 0;
+	int cnt2 = 0;
+	for (int a = 146810; a <= 612564; ++a)
 	{
-		std::istringstream iss(line);
-
 		// part a
-		std::set<std::string> passphrase;
-		std::string tmp;
-		bool valid = true;
-		while (iss >> tmp)
+		int b = a;
+		for (int i = 0; i < 6; ++i)
 		{
-			std::pair<std::set<std::string>::iterator, bool> res = passphrase.insert(tmp);
-			if (!res.second)
-			{
-				valid = false;
-				break;
-			}
+			digits[5-i] = b % 10;
+			b /= 10;
 		}
 
-		if (valid) ++suma;
+		if (digits[0] > digits[1])
+			continue;
+		if (digits[1] > digits[2])
+			continue;
+		if (digits[2] > digits[3])
+			continue;
+		if (digits[3] > digits[4])
+			continue;
+		if (digits[4] > digits[5])
+			continue;
+
+		if (!(digits[0] == digits[1] || digits[1] == digits[2]
+			|| digits[2] == digits[3] || digits[3] == digits[4]
+			|| digits[4] == digits[5]))
+			continue;
+
+		++cnt1;
+
 
 		// part b
-		std::vector<std::string> pp;
-		valid = true;
-		std::istringstream issb(line);
-		while (issb >> tmp)
+		if (digits[0] == digits[1] && digits[1] != digits[2])
 		{
-			// compare with each already processed word in phrase
-			size_t sz = pp.size();
-			for (size_t i = 0; i < sz; ++i)
-			{
-				const std::string& cur = pp[i];
-				size_t nChar = cur.size();
-
-				if (tmp.size() != nChar)
-					continue;
-
-				// save all char frequencies in map for current and new string
-				std::map<char, size_t> mTmp, mCur;
-				for (size_t c = 0; c < nChar; ++c)
-				{
-					std::map<char, size_t>::iterator itTmp, itCur;
-
-					if ((itTmp = mTmp.find(tmp[c])) != mTmp.end())
-						++(itTmp->second);
-					else
-						mTmp[tmp[c]] = 1;
-
-					if ((itCur = mCur.find(cur[c])) != mCur.end())
-						++(itCur->second);
-					else
-						mCur[cur[c]] = 1;
-				}
-
-				// compare frequencies
-				bool unequal = false;
-				std::map<char, size_t>::const_iterator itTmp = mTmp.begin();
-				std::map<char, size_t>::const_iterator itTmpEnd = mTmp.end();
-				for (; itTmp != itTmpEnd; ++itTmp)
-				{
-					char c = itTmp->first;
-					size_t cnt = itTmp->second;
-					std::map<char, size_t>::iterator itCur;
-					if ((itCur = mCur.find(c)) != mCur.end())
-					{
-						if (itCur->second != cnt)
-						{
-							unequal = true;
-							break;
-						}
-					}
-					else
-					{
-						unequal = true;
-						break;
-					}
-				}
-
-
-				if (!unequal)
-				{
-					valid = false;
-					break;
-				}
-			}
-
-			// if no double entry, add word to phrase; otherwise phrase invalid
-			if (valid)
-				pp.push_back(tmp);
-			else
-				break;
+			++cnt2;
+			continue;
 		}
 
-		if (valid)
-			++sumb;
+		if (digits[1] == digits[2] && digits[0] != digits[1]
+			&& digits[2] != digits[3])
+		{
+			++cnt2;
+			continue;
+		}
+
+		if (digits[2] == digits[3] && digits[1] != digits[2]
+			&& digits[3] != digits[4])
+		{
+			++cnt2;
+			continue;
+		}
+
+		if (digits[3] == digits[4] && digits[2] != digits[3]
+			&& digits[4] != digits[5])
+		{
+			++cnt2;
+			continue;
+		}
+
+		if (digits[4] == digits[5] && digits[3] != digits[4])
+		{
+			++cnt2;
+			continue;
+		}
 	}
 
-	writeSolution(suma, sumb);
+	writeSolution(cnt1, cnt2);
 }
 
 
