7d3bc93e2b08117cb4138f48ab972b2eb0a54d57
mbreit <markus.breit@gcsc.uni-frankfurt.de>
2019-12-12 21:05:50 +0100

    cleanup day 11

--- AoC2019/day11.h
+++ AoC2019/day11.h
@@ -8,91 +8,22 @@
 #include "classes/intCodeMachine.h"
 
 
-template <>
-void executeDay<11>(const std::string& fn)
+
+int executeHullPaintingRobot
+(
+	IntCodeMachine<int64_t>& robot,
+	std::vector<int64_t> input,
+	std::vector<int64_t>& output,
+	std::map<std::pair<int, int>, int>& hull,
+	int startingColor
+)
 {
-	std::vector<int64_t> opCode;
-
-	// read program
-	{
-		std::ifstream infile(fn.c_str());
-		int64_t id;
-		while (infile >> id)
-			opCode.push_back(id);
-	}
-
-	int sola = 1;
+	int paintedPanelsOut = 1;
 	std::pair<int, int> curPos = {0, 0};
 	int curDir = 1;
-	std::map<std::pair<int, int>, int> hull;
-	auto hullIt = hull.begin();
-	hullIt = hull.insert(hullIt, std::make_pair(curPos, 0));
-	IntCodeMachine<int64_t> robot(opCode);
-	std::vector<int64_t> input(1, 0);
-	std::vector<int64_t> output;
-	robot.execute(input, output);
-	while (robot.state() == IntCodeMachine<int64_t>::ICMS_WAITING_FOR_INPUT)
-	{
-		if (output.size() != 2)
-		{
-			std::cout << "output size is " << output.size() << " instead of 2." << std::endl;
-			throw std::runtime_error("");
-		}
-
-		// paint the hull
-		hullIt->second = output[0];
-
-
-		// move
-		if (output[1] == 0)
-		{
-			// turn left
-			curDir = (curDir + 1) % 4;
-		}
-		else if (output[1] == 1)
-		{
-			// turn right
-			if (!curDir)
-				curDir = 3;
-			else
-				--curDir;
-		}
-		switch (curDir)
-		{
-			case 0:
-				++curPos.first; break;
-			case 1:
-				++curPos.second; break;
-			case 2:
-				--curPos.first; break;
-			case 3:
-				--curPos.second; break;
-			default: break;
-		}
-
-		// update hull iterator
-		hullIt = hull.lower_bound(curPos);
-		if (hullIt == hull.end() || hullIt->first != curPos)
-		{
-			++sola;
-			hullIt = hull.insert(hullIt, std::make_pair(curPos, 0));
-		}
-
-		// prepare next robot instruction
-		input[0] = hullIt->second;
-		output.clear();
-		robot.execute(input, output);
-	}
-	std::cout << "a: " << sola << std::endl;
-
-	// part b
-	curPos = {0, 0};
-	curDir = 1;
 	hull.clear();
-	hullIt = hull.begin();
-	hullIt = hull.insert(hullIt, std::make_pair(curPos, 1));
-	robot.reset(opCode);
-	robot.setMemorySize(10000);
+	auto hullIt = hull.begin();
+	hullIt = hull.insert(hullIt, std::make_pair(curPos, startingColor));
 	do
 	{
 		// execute robot
@@ -109,19 +40,11 @@ void executeDay<11>(const std::string& fn)
 		hullIt->second = output[0];
 
 		// move
-		if (output[1] == 0)
-		{
-			// turn left
+		if (output[1] == 0) // turn left
 			curDir = (curDir + 1) % 4;
-		}
-		else if (output[1] == 1)
-		{
-			// turn right
-			if (!curDir)
-				curDir = 3;
-			else
-				--curDir;
-		}
+		else if (output[1] == 1) // turn right
+			curDir = (curDir + 3) % 4;
+
 		switch (curDir)
 		{
 			case 0:
@@ -138,9 +61,45 @@ void executeDay<11>(const std::string& fn)
 		// update hull iterator
 		hullIt = hull.lower_bound(curPos);
 		if (hullIt == hull.end() || hullIt->first != curPos)
+		{
 			hullIt = hull.insert(hullIt, std::make_pair(curPos, 0));
+			if (robot.state() == IntCodeMachine<int64_t>::ICMS_WAITING_FOR_INPUT)
+				++paintedPanelsOut;
+		}
 	}
 	while (robot.state() == IntCodeMachine<int64_t>::ICMS_WAITING_FOR_INPUT);
+
+	return paintedPanelsOut;
+}
+
+
+
+template <>
+void executeDay<11>(const std::string& fn)
+{
+	std::vector<int64_t> opCode;
+
+	// read program
+	{
+		std::ifstream infile(fn.c_str());
+		int64_t id;
+		while (infile >> id)
+			opCode.push_back(id);
+	}
+
+	// part a
+	std::map<std::pair<int, int>, int> hull;
+	IntCodeMachine<int64_t> robot(opCode);
+	std::vector<int64_t> input(1, 0);
+	std::vector<int64_t> output;
+	int sola = executeHullPaintingRobot(robot, input, output, hull, 0);
+	std::cout << "a: " << sola << std::endl;
+
+
+	// part b
+	robot.reset(opCode);
+	robot.setMemorySize(10000);
+	executeHullPaintingRobot(robot, input, output, hull, 1);
 
 	// now find max extensions and copy to arrays
 	int minX = std::numeric_limits<int>::max();
@@ -148,7 +107,7 @@ void executeDay<11>(const std::string& fn)
 	int minY = std::numeric_limits<int>::max();
 	int maxY = std::numeric_limits<int>::min();
 	auto hullItEnd = hull.end();
-	for (hullIt = hull.begin(); hullIt != hullItEnd; ++hullIt)
+	for (auto hullIt = hull.begin(); hullIt != hullItEnd; ++hullIt)
 	{
 		minX = std::min(minX, hullIt->first.first);
 		minY = std::min(minY, hullIt->first.second);
@@ -159,7 +118,7 @@ void executeDay<11>(const std::string& fn)
 	const std::size_t nCols = maxX - minX + 1;
 
 	std::vector<std::vector<char> > paintedHull(nRows, std::vector<char>(nCols, ' '));
-	for (hullIt = hull.begin(); hullIt != hullItEnd; ++hullIt)
+	for (auto hullIt = hull.begin(); hullIt != hullItEnd; ++hullIt)
 		if (hullIt->second)
 			paintedHull[maxY - hullIt->first.second][hullIt->first.first - minX] = '#';
 
