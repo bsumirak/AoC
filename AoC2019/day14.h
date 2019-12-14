3c6046e968abac002f21847f5380e1491ee7dbb9
mbreit <markus.breit@gcsc.uni-frankfurt.de>
2019-12-14 09:17:53 +0100

    day 14

--- AoC2019/day14.h
+++ AoC2019/day14.h
@@ -5,161 +5,153 @@
  *      Author: mbreit
  */
 
+struct Reaction
+{
+	std::vector<std::string> name;
+	std::vector<int64_t> quantity;
+};
+
+
+struct Educt
+{
+	Educt (std::string n, int64_t q) :
+		name(n), quantity(q) {};
+
+	std::string name;
+	int64_t quantity;
+};
+
+
+int64_t requiredOre
+(
+	int64_t fuel,
+	const std::map<std::string, Reaction>& reactionMap,
+	const std::map<std::string, std::set<std::string> >& _edOf
+)
+{
+	std::map<std::string, std::set<std::string> > edOf(_edOf);
+
+	std::map<std::string, int64_t> reqAmount;
+	auto it = reactionMap.begin();
+	auto itEnd = reactionMap.end();
+	for (; it != itEnd; ++it)
+		reqAmount[it->first] = 0;
+
+	reqAmount["FUEL"] = fuel;
+
+	while (true)
+	{
+		auto ite = edOf.begin();
+		while (ite != edOf.end() && (ite->second.size() || !reqAmount[ite->first]))
+			++ite;
+
+		if (ite == edOf.end())
+			break;
+
+		const std::string& prod = ite->first;
+
+		if (prod == "ORE")
+			break;
+
+		//std::cout << "required amout of " << prod << ": " << reqAmount[prod] << std::endl;
+		auto itr = reactionMap.find(prod);
+		int64_t amount = reqAmount[prod] / itr->second.quantity.back();
+		if (reqAmount[prod] % itr->second.quantity.back())
+			++amount;
+
+		//std::cout << "amount: " << reqAmount[prod] << " " << itr->second.quantity.back() << std::endl;
+
+		for (std::size_t i = 0; i < itr->second.name.size() - 1; ++i)
+			reqAmount[itr->second.name[i]] += amount * itr->second.quantity[i];
+
+		reqAmount[prod] = 0;
+
+		ite = edOf.begin();
+		auto iteEnd = edOf.end();
+		for (; ite != iteEnd; ++ite)
+		{
+			auto itp = ite->second.find(prod);
+			if (itp != ite->second.end())
+				ite->second.erase(itp);
+		}
+	}
+
+	return reqAmount["ORE"];
+}
+
+
 
 template <>
 void executeDay<14>(const std::string& fn)
 {
-	std::string input = "jxqlasbh";
+	std::ifstream infile(fn.c_str());
 
-	size_t ones = 0;
-	std::vector<std::vector<size_t> > mem(128);
-	for (size_t i = 0; i < 128; ++i)
-		mem[i].resize(128, 0);
-
-	for (size_t i = 0; i < 128; ++i)
+	std::string line;
+	std::map<std::string, Reaction> reactionMap;
+	while (std::getline(infile, line))
 	{
-		std::ostringstream oss;
-		oss << input << "-" << i;
+		std::istringstream iss(line);
 
-		std::string knotHash = calculate_knot_hash(oss.str());
-
-		// count ones in knot hash
-		std::vector<size_t>& row = mem[i];
-		for (size_t j = 0; j < 32; ++j)
+		Reaction ed;
+		int tmp;
+		while (iss >> tmp)
 		{
-			switch (knotHash[j])
-			{
-				case '0':
-					break;
-				case '1':
-					ones += 1;
-					row[4*j+3] = 1;
-					break;
-				case '2':
-					ones += 1;
-					row[4*j+2] = 1;
-					break;
-				case '3':
-					ones += 2;
-					row[4*j+3] = 1;
-					row[4*j+2] = 1;
-					break;
-				case '4':
-					ones += 1;
-					row[4*j+1] = 1;
-					break;
-				case '5':
-					ones += 2;
-					row[4*j+3] = 1;
-					row[4*j+1] = 1;
-					break;
-				case '6':
-					ones += 2;
-					row[4*j+2] = 1;
-					row[4*j+1] = 1;
-					break;
-				case '7':
-					ones += 3;
-					row[4*j+3] = 1;
-					row[4*j+2] = 1;
-					row[4*j+1] = 1;
-					break;
-				case '8':
-					ones += 1;
-					row[4*j] = 1;
-					break;
-				case '9':
-					ones += 2;
-					row[4*j+3] = 1;
-					row[4*j] = 1;
-					break;
-				case 'a':
-					ones += 2;
-					row[4*j+2] = 1;
-					row[4*j] = 1;
-					break;
-				case 'b':
-					ones += 3;
-					row[4*j+3] = 1;
-					row[4*j+2] = 1;
-					row[4*j] = 1;
-					break;
-				case 'c':
-					ones += 2;
-					row[4*j+1] = 1;
-					row[4*j] = 1;
-					break;
-				case 'd':
-					ones += 3;
-					row[4*j+3] = 1;
-					row[4*j+1] = 1;
-					row[4*j] = 1;
-					break;
-				case 'e':
-					ones += 3;
-					row[4*j+2] = 1;
-					row[4*j+1] = 1;
-					row[4*j] = 1;
-					break;
-				case 'f':
-					ones += 4;
-					row[4*j+3] = 1;
-					row[4*j+2] = 1;
-					row[4*j+1] = 1;
-					row[4*j] = 1;
-					break;
-				default:
-					std::cout << "Unexpected character in knot hash." << std::endl;
-					throw;
-			}
+			ed.name.resize(ed.name.size() + 1);
+			ed.quantity.resize(ed.quantity.size() + 1);
+			ed.quantity.back() = tmp;
+			iss >> ed.name.back();
 		}
+		if (reactionMap.find(ed.name.back()) != reactionMap.end())
+		{
+			std::cout << "bflkgj" << std::endl;
+			throw std::runtime_error("");
+		}
+		reactionMap[ed.name.back()] = ed;
 	}
 
-	// analyze contiguity
-	size_t regCnt = 1;
-	for (size_t i = 0; i < 128; ++i)
+	// build depender list
+	std::map<std::string, std::set<std::string> > edOf;
+	auto it = reactionMap.begin();
+	auto itEnd = reactionMap.end();
+	for (; it != itEnd; ++it)
 	{
-		for (size_t j = 0; j < 128; ++j)
-		{
-			if (mem[i][j] != 1)
-				continue;
-
-			// start a new region
-			++regCnt;
-			std::queue<std::pair<size_t, size_t> > q;
-			q.push(std::make_pair(i,j));
-
-			// find whole region
-			while (!q.empty())
-			{
-				const std::pair<size_t, size_t>& p = q.front();
-				size_t k = p.first;
-				size_t l = p.second;
-				q.pop();
-
-				// check for 1
-				if (mem[k][l] != 1)
-					continue;
-
-				// assign region
-				mem[k][l] = regCnt;
-
-				// look left, right, top, bottom
-				if (k > 0)
-					q.push(std::make_pair(k-1, l));
-				if (k < 127)
-					q.push(std::make_pair(k+1, l));
-				if (l > 0)
-					q.push(std::make_pair(k, l-1));
-				if (l < 127)
-					q.push(std::make_pair(k, l+1));
-			}
-		}
+		const std::vector<std::string>& eds = it->second.name;
+		const std::size_t nEds = eds.size() - 1;
+		for (std::size_t e = 0; e < nEds; ++e)
+			edOf[eds[e]].insert(it->first);
 	}
+	edOf["FUEL"];
 
-	--regCnt;
 
-	writeSolution(ones, regCnt);
+	// part a
+	int64_t sola = requiredOre(1, reactionMap, edOf);
+
+
+	// part b
+	int64_t ore(1000000000000);
+	int64_t prop = ore / int64_t(sola);
+	int64_t guess = prop;
+	while (requiredOre(guess, reactionMap, edOf) <= ore)
+		guess += prop;
+
+	std::vector<int64_t> check(prop+1);
+	for (std::size_t i = 0; i < prop+1; ++i)
+		check[i] = guess - prop + i;
+
+	auto cmpFct = [&] (const int64_t& fuelA, const int64_t& fuelB) -> bool
+	{
+		// little hack :)
+		if (fuelA == ore)
+			return fuelA < requiredOre(fuelB, reactionMap, edOf);
+		if (fuelB == ore)
+			return requiredOre(fuelA, reactionMap, edOf) < fuelB;
+		return requiredOre(fuelA, reactionMap, edOf) < requiredOre(fuelB, reactionMap, edOf);
+	};
+
+	auto solb = std::equal_range(check.begin(), check.end(), ore, cmpFct);
+
+
+	writeSolution(sola, *(solb.second)-1);
 }
 
 
