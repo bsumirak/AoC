13db0aed061aebff0a8eb19ef5ee8f5b46b24ace
mbreit <markus.breit@gcsc.uni-frankfurt.de>
2020-12-18 07:23:19 +0100

    day 18 - ugly calculator parser

---
+++ AoC2020/day18.h
@@ -0,0 +1,178 @@
+/*
+ * day18.h
+ *
+ *  Created on: 2020-12-18
+ *      Author: mbreit
+ */
+
+
+template <>
+void executeDay<18>(const std::string& fn)
+{
+	// read input
+	uint64_t resA = 0;
+	{
+		std::ifstream infile(fn.c_str());
+		std::string line;
+		while (std::getline(infile, line))
+		{
+			std::istringstream iss(line);
+			std::string op;
+			std::stack<uint64_t> numberStack;
+			numberStack.push(0);
+			std::stack<char> opStack;
+			opStack.push('+');
+			while (iss >> op)
+			{
+				if (op == "(")
+				{
+					numberStack.push(0);
+					opStack.push('+');
+					continue;
+				}
+				if (op == ")")
+				{
+					uint64_t res = numberStack.top();
+					char c = opStack.top();
+
+					numberStack.pop();
+					opStack.pop();
+
+					if (c == '+')
+						numberStack.top() += res;
+					else if (c == '*')
+						numberStack.top() *= res;
+
+					continue;
+				}
+				if (op == "+")
+				{
+					opStack.push('+');
+					continue;
+				}
+				if (op == "*")
+				{
+					opStack.push('*');
+					continue;
+				}
+				uint64_t val = stoull(op);
+				char c = opStack.top();
+				opStack.pop();
+
+				if (c == '+')
+					numberStack.top() += val;
+				else if (c == '*')
+					numberStack.top() *= val;
+			}
+			resA += numberStack.top();
+		}
+		infile.close();
+	}
+
+
+
+	// part b
+	uint64_t resB = 0;
+	{
+		std::ifstream infile(fn.c_str());
+		std::string line;
+		while (std::getline(infile, line))
+		{
+			std::istringstream iss(line);
+			std::string op;
+			std::stack<uint64_t> numberStack;
+			numberStack.push(0);
+			std::stack<bool> groupStack;
+			groupStack.push(false);
+			std::stack<char> opStack;
+			opStack.push('+');
+			while (iss >> op)
+			{
+				if (op == "(")
+				{
+					numberStack.push(0);
+					groupStack.push(false);
+					opStack.push('+');
+					continue;
+				}
+				if (op == ")")
+				{
+					// close pseudo-parentheses
+					if (groupStack.top())
+					{
+						uint64_t res = numberStack.top();
+						numberStack.pop();
+						opStack.pop();
+						numberStack.top() *= res;
+					}
+					groupStack.pop();
+
+					uint64_t res = numberStack.top();
+					char c = opStack.top();
+
+					numberStack.pop();
+					opStack.pop();
+
+					if (c == '+')
+						numberStack.top() += res;
+					else if (c == '*')
+						numberStack.top() *= res;
+
+					continue;
+				}
+				if (op == "+")
+				{
+					opStack.push('+');
+					continue;
+				}
+				if (op == "*")
+				{
+					// close pseudo-parentheses
+					if (groupStack.top())
+					{
+						uint64_t res = numberStack.top();
+						numberStack.pop();
+						opStack.pop();
+						numberStack.top() *= res;
+					}
+
+					opStack.push('*');
+
+					// open pseudo-parentheses
+					numberStack.push(0);
+					groupStack.top() = true;
+					opStack.push('+');
+
+					continue;
+				}
+				uint64_t val = stoull(op);
+				char c = opStack.top();
+				opStack.pop();
+
+				if (c == '+')
+					numberStack.top() += val;
+				else if (c == '*')
+					numberStack.top() *= val;
+			}
+
+			// close pseudo-parentheses
+			if (groupStack.top())
+			{
+				uint64_t res = numberStack.top();
+				numberStack.pop();
+				opStack.pop();
+				numberStack.top() *= res;
+			}
+			groupStack.pop();
+
+			resB += numberStack.top();
+		}
+		infile.close();
+	}
+
+
+
+	writeSolution(resA, resB);
+}
+
+
