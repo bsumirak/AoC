3bbeb0523b5fd1c0cfd6d34c86a1729bfb295da5
mbreit <markus.breit@gcsc.uni-frankfurt.de>
2022-12-03 12:14:42 +0100

    minor simplification day 02

--- AoC2022/day02.h
+++ AoC2022/day02.h
@@ -10,39 +10,26 @@ template <>
 void executeDay<2>(const std::string& fn)
 {
 	// read input
-	std::array<unsigned, 2500> opp;
-	std::array<unsigned, 2500> my;
-
 	std::ifstream infile(fn.c_str());
 	char c;
-	for (unsigned i = 0; i < 2500; ++i)
-	{
-		infile >> c;
-		opp[i] = c - 'A';
-		infile >> c;
-		my[i] = c - 'X';
-	}
-	infile.close();
-
-	// part a
 	unsigned resA = 0;
-	for (unsigned i = 0; i < 2500; ++i)
-	{
-		resA += my[i] + 1u;
-		if (my[i] == opp[i])
-			resA += 3;
-		else if (my[i] == (opp[i] + 1u) % 3u)
-			resA += 6;
-	}
-
-	// part b
 	unsigned resB = 0;
 	for (unsigned i = 0; i < 2500; ++i)
 	{
-		resB += 3u * my[i];
-		resB += (opp[i] + (my[i] + 2u) % 3u) % 3u + 1u;
-	}
+		infile >> c;
+		unsigned opp = c - 'A';
+		infile >> c;
+		unsigned my = c - 'X';
 
+		// part a
+		resA += my + 1u;
+		resA += 3u * ((4u + my - opp) % 3u);
+
+		// part b
+		resB += 3u * my;
+		resB += (opp + (my + 2u) % 3u) % 3u + 1u;
+	}
+	infile.close();
 
 	writeSolution(resA, resB);
 }
